00aa18742c9e2759b3cdcb38c7b5ce49573451a4
PapaOG-dev <msu-hcooley5@mcneese.edu>
2026-04-15 17:35:54 -0500

    changes

--- CheckOut.php
+++ CheckOut.php
@@ -7,6 +7,52 @@
         $priceQuery = $db->query("SELECT SUM(price) AS total_price FROM Products");
         $row = $priceQuery->fetch(PDO::FETCH_ASSOC);
         $total = 0;
+        
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            $card_number = $_POST['card_number'];
+            $expiration_date = $_POST['expiration_date'];
+            $ccv = $_POST['ccv'];
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $zip_code = $_POST['zip_code'];
+            $street_address = $_POST['street_address'];
+            $state = $_POST['state'];
+            $city = $_POST['city'];
+
+            
+
+            $statement = $db->prepare(
+                    "SELECT * 
+                     FROM CreditCards
+                     WHERE card_number = :card_number
+                     AND ccv = :ccv
+                     AND expiration_date = :expiration_date"
+                    );
+
+            $statement->bindValue(':card_number', $card_number,PDO::PARAM_STR );
+            $statement->bindValue(':expiration_date', $expiration_date,PDO::PARAM_STR);
+            $statement->bindValue(':ccv', $ccv, );
+            //$statement->bindValue(':first_name', $first_name,PDO::PARAM_STR );
+            //$statement->bindValue(':last_name', $last_name, PDO::PARAM_STR);
+            //$statement->bindValue(':zip_code', $zip_code, PDO::PARAM_STR);
+            //$statement->bindValue(':street_address', $street_address,PDO::PARAM_STR);
+            //$statement->bindValue(':state', $state,PDO::PARAM_STR);
+            //$statement->bindValue(':city', $city,PDO::PARAM_STR);
+
+            $statement->execute();
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if($row) {
+                echo "<script>window.onload = function () {
+                successPopup();};</script>";
+            }else{
+                echo "<script>
+                window.onload = function() {
+                    errorPopup();
+                };    
+                </script>;";
+            }
+        }
       
 ?>
 
@@ -41,7 +87,7 @@
         </header>
         
         <main>
-            <form method = "POST" action = "cart.php">
+            <form method = "POST" action = "checkout.php">
             <div class="CartItemsContainer">                
                 <table class="CartItemsTable">
                     <thead>
@@ -75,10 +121,49 @@
                             </thead>
                             <tbody>                                
                                 <tr>                            
-                                    <td class="titlecell">                                
-                                        <p>Total:</p>                                
-                                    </td>
-                                    <td>$<span id = "total">0.00</span></td>
+                                    <td colspan="2">                                
+                                        <input id="CardNumber" name='card_number' type="text" maxlength='19' placeholder="Card Number" required>                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input  id='expiration' type="text" maxlength='10' name='expiration_date' placeholder="Expiration YY-MMM eg:29-Mar" required>                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input id='Ccv'type="text" name='ccv' maxlength='3' placeholder="CCV" required>                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input type="text" name='first_name' placeholder="First Name">                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input type="text" name='last_name' placeholder="Last Name">                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input type="text" name='street_address' placeholder="Street Address">                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input type="text" name='zip_code' placeholder="ZIP Code">                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input type="text" name='city' placeholder="City">                                
+                                    </td>                                    
+                                </tr>
+                                <tr>                            
+                                    <td colspan="2">                                
+                                        <input type="text" name='state' placeholder="State">                                
+                                    </td>                                    
                                 </tr>
                             </tbody>
                         </table>
